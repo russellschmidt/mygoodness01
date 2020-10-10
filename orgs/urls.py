6df81ed63dc2a9eba67fdc0d994391ab596ddfc5
Russell Schmidt <russell.schmidt@me.com>
2020-10-10 09:49:49 -0700

    add detail view

--- orgs/urls.py
+++ orgs/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  # ex: /orgs/5/
+  path('<int:org_id>/', views.detail, name='detail'),
 ]
--- orgs/views.py
+++ orgs/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
-
 def index(request):
     return HttpResponse("Hello, world. You're at the orgs index.")
+
+def detail(request, org_id):
+    return HttpResponse("You're looking at org %s." % org_id)
