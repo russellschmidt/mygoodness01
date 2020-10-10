9f78f39d2c4c57f426cc12dbe330d320bff3b79a
Russell Schmidt <russell.schmidt@me.com>
2020-10-09 23:20:54 -0700

    add org cards to main feed

--- orgs/models.py
+++ orgs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Org(models.Model):
+  category_text = models.CharField(max_length=40)
+  title_text = models.CharField(max_length=40)
+  subtitle_text = models.CharField(max_length=200, null=True)
+  button_label = models.CharField(max_length=20, default="Learn More")
+  background_image = models.URLField(max_length=200, default='https://source.unsplash.com/random/480x853')
+  pub_date = models.DateTimeField('date published')
